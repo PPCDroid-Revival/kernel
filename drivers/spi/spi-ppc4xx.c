84b08711e1516640e581fd8e2a8e0cbef3d8707e
Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>
2012-05-28 22:18:54 +0400

    ppc4xx: spi driver trivial fixes
    Signed-off-by: Dmitry Eremin-Solenikov <dmitry_eremin@mentor.com>

--- drivers/spi/spi-ppc4xx.c
+++ drivers/spi/spi-ppc4xx.c
@@ -390,7 +390,7 @@ static void free_gpios(struct ppc4xx_spi *hw)
 /*
  * platform_device layer stuff...
  */
-static int __init spi_ppc4xx_of_probe(struct platform_device *op)
+static int __devinit spi_ppc4xx_of_probe(struct platform_device *op)
 {
 	struct ppc4xx_spi *hw;
 	struct spi_master *master;
@@ -564,7 +564,7 @@ free_master:
 	return ret;
 }
 
-static int __exit spi_ppc4xx_of_remove(struct platform_device *op)
+static int __devexit spi_ppc4xx_of_remove(struct platform_device *op)
 {
 	struct spi_master *master = dev_get_drvdata(&op->dev);
 	struct ppc4xx_spi *hw = spi_master_get_devdata(master);
@@ -587,7 +587,7 @@ MODULE_DEVICE_TABLE(of, spi_ppc4xx_of_match);
 
 static struct platform_driver spi_ppc4xx_of_driver = {
 	.probe = spi_ppc4xx_of_probe,
-	.remove = __exit_p(spi_ppc4xx_of_remove),
+	.remove = __devexit_p(spi_ppc4xx_of_remove),
 	.driver = {
 		.name = DRIVER_NAME,
 		.owner = THIS_MODULE,
